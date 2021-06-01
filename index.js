72301697bde68924030ed2cbd8a39e71f74b1af1
samadabdul1 <80896278+samadabdul1@users.noreply.github.com>
2021-06-01 20:06:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const port = 7000;
+const db = require('./config/mongoose');
+const  Task  = require('./models/task');
+const app = express();
+app.use(express.static("./views"));
+app.use(express.urlencoded());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.get('/', function(req, res){
+    Task.find({}, function(err, task){
+        if(err){
+            console.log('Error in fetching tasks from db');
+            return;
+        }
+
+        return res.render('home', {
+            tittle: "Home",
+            task: task
+        });
+    }
+)});
+app.post('/create-task', function(req, res){
+    
+    Task.create({
+        description: req.body.description,
+        category: req.body.category,
+        date: req.body.date
+        }, function(err, newtask){
+        if(err){console.log('error in creating task', err); return;}
+        
+
+        //console.log(newtask);
+        return res.redirect('back');
+
+    });
+});
+
+
+// deleting Tasks
+app.get('/delete-task', function(req, res){
+    // get the id from query
+    var id = req.query;
+
+    // checking the number of tasks selected to delete
+    var count = Object.keys(id).length;
+    for(let i=0; i < count ; i++){
+        
+        // finding and deleting tasks from the DB one by one using id
+        Task.findByIdAndDelete(Object.keys(id)[i], function(err){
+        if(err){
+            console.log('error in deleting task');
+            }
+        })
+    }
+    return res.redirect('back'); 
+});
+
+// make the app to listen on asigned port number
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server : ${err}`);
+    }
+
+    console.log(`Server is running on port : ${port}`);
+});
